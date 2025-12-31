35cb3e8e7ffb2e0069929e7570ee168c37d2c054
arcbhattx <arcbh11@gmail.com>
2025-12-31 13:44:15 -0800

    sidebar componenet integrtion

--- resume-fill/app/components/Sidebar.tsx
+++ resume-fill/app/components/Sidebar.tsx
@@ -1,5 +1,22 @@
+import SidebarItem from "./SidebarItem";
 
+import {Home, Settings, BookOpenCheck, Book } from "lucide-react";
+export default function Sidebar() {
+  return (
+    <aside className="h-screen w-64 border-r border-gray-200 bg-white p-6 flex flex-col">
+      <h2 className="text-xl font-bold font-mono tracking-tight mb-8 text-black">
+        Resume Fill
+      </h2>
 
-export default function SideBar(){
+      <nav className="flex flex-col gap-1">
+        <SidebarItem label="Home" href="/dashboard"  icon ={<Home/>}/>
+        <SidebarItem label="Job Recs" href="/dashboard/applications" notificationCount={3} icon={<BookOpenCheck/>}/>
+        <SidebarItem label="Settings" href="/dashboard/settings"  icon = {<Settings/>}/>
+      </nav>
 
+      <div className="mt-auto text-gray-500 text-sm border-t border-gray-200 pt-6">
+        © 2025 — Job Hunt Smart 🚀
+      </div>
+    </aside>
+  );
 }
--- resume-fill/app/components/SidebarItem.tsx
+++ resume-fill/app/components/SidebarItem.tsx
@@ -1,3 +1,47 @@
-export default function SideBarItem(){
-    
+"use client";
+
+import Link from "next/link";
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+
+interface SidebarItemProps {
+  label: string;
+  href: string;
+  icon?: ReactNode;
+  notificationCount?: number;
+}
+
+export default function SidebarItem({
+  label,
+  href,
+  icon,
+  notificationCount = 0,
+}: SidebarItemProps) {
+  const pathname = usePathname();
+  const active = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center justify-between px-4 py-1 transition-all
+      ${
+        active
+          ? "bg-black text-white border-black shadow-md"
+          : "bg-white border-gray-200 text-gray-700 hover:bg-gray-100 hover:-translate-y-[1px]"
+      }`}
+    >
+      {/* Left side: icon + label */}
+      <div className="flex items-center gap-3">
+        {icon && <span className="text-xl">{icon}</span>}
+        <span className="font-semibold tracking-wide">{label}</span>
+      </div>
+
+      {/* Right side: notification bubble */}
+      {notificationCount > 0 && (
+        <span className="text-xs px-2 py-0.5 rounded-full bg-black text-white">
+          {notificationCount}
+        </span>
+      )}
+    </Link>
+  );
 }
